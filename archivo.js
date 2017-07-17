af9868838c1b14e7d73bbf5e9521d4420a5dfb5c
gabiprds93 <gabiprds.93@gmail.com>
2017-07-17 14:11:02 -0500

    Segunda parte

--- archivo.js
+++ archivo.js
@@ -9,8 +9,17 @@ pVenus = pVenus.toFixed(2);
 var pJupiter = peso * 2.55;
 pJupiter = pJupiter.toFixed(2);
 
+var pUrano = peso * 0.99;
+pUrano = pUrano.toFixed(2);
 
-alert("Tu peso en Mercurio es: "+pMercurio+"\n"+ "Tu peso en Venus es: "+pVenus+"\n"+"Tu peso en Jupiter es: " +pJupiter);
+var pMarte = peso *0.38;
+pMarte = pMarte.toFixed(2);
+
+var pSaturno = peso * 0.93;
+pSaturno = pSaturno.toFixed(2);
+
+
+alert("Tu peso en Mercurio es: "+pMercurio+"\n"+ "Tu peso en Venus es: "+pVenus+"\n"+"Tu peso en Jupiter es: " +pJupiter +"\n"+"Tu peso en Urano es: "+pUrano+"\n"+ "tu peso en Marte es: "+pMarte+"\n"+ "Tu peso en Saturno es: "+pSaturno);
 
 }
 calcularPeso();
